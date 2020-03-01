680dda978228e528bdabd1232269afdde3602f19
LuQ232 <LuQ232>
2020-03-02 00:40:50 +0100

    Templates done, header file no more used

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
-#include "sort.h"
+#include <memory>
+//#include "sort.h"
+#include "sort.cpp"
 
 
 int main()
 {
-    sort test; //SORT OBJECT
-    test.show(); // SORT FUNCTION
+
+    Sort test; //SORT OBJECT
+
+    int a=0;
+    double b=0.5;
+    int c=5;
+    float d=0.01;
+    test.show(a); // SORT FUNCTION
+    test.merge_sort(b);
+    test.quick_sort(c);
+    test.intro_sort(d);
+
+
+    std::cout<<"Array Size: ";
+    int size;
+    std::cin>>size;
+    std::cout<<std::endl;
+
+
+    auto array = std::make_unique<int[]>(size);  // Dynmic memory allocation for array
+                                                 // smart pointer DONT need DELETE
+        for (int i=0; i<size; i++)
+        {
+            array[i]=i;
+            std::cout<<array[i]<<std::endl;
+        }
     return 0;
 }
--- sort.cpp
+++ sort.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include "sort.h"
 
-void sort::show()
-{
-    std::cout<<"TEST"<<std::endl;
 
+
+class Sort
+    {
+
+        public:
+            template<typename Var>
+            static void show(Var test);
+
+            template<typename Var>
+            static void merge_sort(Var test);
+
+            template<typename Var>
+            static void quick_sort(Var test);
+
+            template<typename Var>
+            static void intro_sort(Var test);
+
+    };
+
+
+template<typename Var>
+void Sort::show(Var test)
+{
+    std::cout<<"TEST, VARIABLE ->>>  "<<test<<std::endl;
+}
+
+template<typename Var>
+void Sort::merge_sort(Var test)
+{
+    std::cout<<"Merge Sort, VARIABLE ->>>  "<<test<<std::endl;
+}
+
+template<typename Var>
+void Sort::quick_sort(Var test)
+{
+    std::cout<<"Quick Sort, VARIABLE ->>>  "<<test<<std::endl;
+}
+
+template<typename Var>
+void Sort::intro_sort(Var test)
+{
+    std::cout<<"Intro Sort, VARIABLE ->>>  "<<test<<std::endl;
 }
