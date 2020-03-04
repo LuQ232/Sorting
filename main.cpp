a74860321121d64e9deb1204e6807d55c8d71caa
LuQ232 <LuQ232>
2020-03-04 22:00:31 +0100

    100 arrays of given size works, all sortings.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,13 @@
 
 #include "sort.hpp"
 #include "timer.hpp"
-void display_array(int *array,int last_index)
+
+#define NUMBER_OF_ARRAYS 100
+
+
+void display_array(int *array,int size_of_array)
 {
-    for (int i=0;i<last_index;i++)
+    for (int i=0;i<size_of_array;i++)
     {
         std::cout<<array[i]<<", ";
     }
@@ -18,49 +22,61 @@ void display_array(int *array,int last_index)
 int main()
 {
     Timer timer;
+    Timer timer_of_all_sortings;
     std::cout<<"Array Size: ";
     int size;
     std::cin>>size;
     std::cout<<std::endl;
 
-    int *array_for_quick_sort = new int[size];                           // Dynmic memory allocation for array
-    int *array_for_merge_sort = new int[size];
-    int *array_for_intro_sort = new int[size];
 
-        for (int i=0; i<size; i++)
+
+    //MAKES 100 arrays
+    int **array_of_arrays = new int *[NUMBER_OF_ARRAYS];
+    for (int i=0; i<NUMBER_OF_ARRAYS; i++)
         {
-            array_for_quick_sort[i]=rand() % 100000;
-            array_for_merge_sort[i] = array_for_quick_sort[i];
-            array_for_intro_sort[i] = array_for_quick_sort[i];
+            array_of_arrays[i] = new int[size];
+
         }
-        std::cout<<std::endl;
 
-    std::cout<<"AFTER MERGE SORT VVVV"<<std::endl;
-    timer.start();
-    merge_sort(array_for_merge_sort,0,size-1);
-    timer.stop();
-    timer.print_time_duration();
-    std::cout<<std::endl;
-
-    std::cout<<"AFTER QUICK SORT VVVV"<<std::endl;
-    timer.start();
-    quick_sort(array_for_quick_sort,0,size-1);
-    timer.stop();
-    timer.print_time_duration();
-
-    std::cout<<std::endl;
-
-    std::cout<<"AFTER INTRO SORT!!"<<std::endl;
-    timer.start();
-    intro_sort(array_for_intro_sort,size);
-    timer.stop();
-    timer.print_time_duration();
-    std::cout<<std::endl;
+    //FILL EVERY ARRAY WITH RAND
+    for (int i=0; i<NUMBER_OF_ARRAYS; i++)
+        {
+            for (int j=0; j<size; j++)
+            {
+                array_of_arrays[i][j]=rand() % 100000;
+            }
+        }
 
 
+        timer_of_all_sortings.start();  // START TIMER OF 100 SORTS
+    for (int i=0; i<NUMBER_OF_ARRAYS; i++)
+        {
+            std::cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<std::endl;
+            std::cout<<"Sort number: "<<i<<std::endl;
+            std::cout<<"BEFORE SORT: "<<std::endl;
+           // display_array(array_of_arrays[i],size);
+            timer.start();
+            quick_sort(array_of_arrays[i],0,size-1);
+            timer.stop();
+            timer.print_time_duration();
+            std::cout<<"AFTER SORT: "<<std::endl;
+           // display_array(array_of_arrays[i],size);
+            std::cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<std::endl;
 
-    delete [] array_for_quick_sort;                                     // Remember to deallocate memory for array
-    delete [] array_for_merge_sort;
-    delete [] array_for_intro_sort;
+        }
+        timer_of_all_sortings.stop();       // STOP TIMER OF 100 SORTS
+        timer_of_all_sortings.print_time_duration();
+
+        //TEST - EACH ARRRAY 1 MILION LONG
+        //MERGE 56 SEKUND
+        //QUICK 17 SEKUND
+        //INTRO 52 SEKUND
+
+        for (int i=0; i<NUMBER_OF_ARRAYS; i++)
+        {
+           delete [] array_of_arrays[i];
+        }
+        delete [] array_of_arrays;
+
     return 0;
 }
