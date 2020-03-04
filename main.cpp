1db0bfbd03eef26ae11cbae71b8e077d3011bea7
LuQ232 <LuQ232>
2020-03-04 19:37:23 +0100

    Intro sort done. Timer done. Merge sort bug SOLVED!

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <utility>
 
 #include "sort.hpp"
-
+#include "timer.hpp"
 void display_array(int *array,int last_index)
 {
     for (int i=0;i<last_index;i++)
@@ -17,28 +17,50 @@ void display_array(int *array,int last_index)
 
 int main()
 {
+    Timer timer;
     std::cout<<"Array Size: ";
     int size;
     std::cin>>size;
     std::cout<<std::endl;
 
-    int *array = new int[size];                 // Dynmic memory allocation for array
+    int *array_for_quick_sort = new int[size];                           // Dynmic memory allocation for array
+    int *array_for_merge_sort = new int[size];
+    int *array_for_intro_sort = new int[size];
 
         for (int i=0; i<size; i++)
         {
-            array[i]=rand() % 100000;
+            array_for_quick_sort[i]=rand() % 100000;
+            array_for_merge_sort[i] = array_for_quick_sort[i];
+            array_for_intro_sort[i] = array_for_quick_sort[i];
         }
         std::cout<<std::endl;
-    //display_array(array,size-1);
-    quick_sort(array,0,size-1);
-    std::cout<<"AFTTER QUICK SORT!!"<<std::endl;
-    display_array(array,size-1);
 
- //  std::cout<<"AFTTER MERGE SORT VVVVVVVVVVVVVV!!"<<std::endl;
+    std::cout<<"AFTER MERGE SORT VVVV"<<std::endl;
+    timer.start();
+    merge_sort(array_for_merge_sort,0,size-1);
+    timer.stop();
+    timer.print_time_duration();
+    std::cout<<std::endl;
 
- //   display_array(array,size-1);
+    std::cout<<"AFTER QUICK SORT VVVV"<<std::endl;
+    timer.start();
+    quick_sort(array_for_quick_sort,0,size-1);
+    timer.stop();
+    timer.print_time_duration();
 
-    delete [] array;                        // Remember to deallocate memory for array
+    std::cout<<std::endl;
 
+    std::cout<<"AFTER INTRO SORT!!"<<std::endl;
+    timer.start();
+    intro_sort(array_for_intro_sort,size);
+    timer.stop();
+    timer.print_time_duration();
+    std::cout<<std::endl;
+
+
+
+    delete [] array_for_quick_sort;                                     // Remember to deallocate memory for array
+    delete [] array_for_merge_sort;
+    delete [] array_for_intro_sort;
     return 0;
 }
--- sort.hpp
+++ sort.hpp
@@ -1,22 +1,30 @@
 #include <iostream>
-
+#include <math.h>
+#include <algorithm>
 
 
 template<typename Var>
-void show(Var test)
+void swap(Var &first, Var &second )
 {
-    std::cout<<"TEST, VARIABLE ->>>  "<<test<<std::endl;
+
+    Var tmp = first;
+    first = second;
+    second = tmp;
 }
+
+
 
 template<typename Var>
 void merge(Var *array,int start_index, int middle_index, int end_index)
 {
-    int left_array_size, right_array_size;
+    Var left_array_size, right_array_size;
 
     left_array_size = middle_index-start_index+1;
     right_array_size = end_index-middle_index;
 
-    int left_array[left_array_size], right_array[right_array_size];
+    Var *left_array = new Var[left_array_size];
+    Var *right_array = new Var[right_array_size];
+
 
     //////////////////FILLING BOTH ARRAYS/////////////////////////
 
@@ -58,30 +66,22 @@ void merge(Var *array,int start_index, int middle_index, int end_index)
         k++;
     }
 
+    delete [] left_array;
+    delete [] right_array;
 }
 
 template<typename Var>
 void merge_sort(Var *array,int start_index,int end_index)
 {
-    int middle_index;
-    if( start_index < end_index )
+    if( start_index < end_index )                   // do while we get 1 part of array
     {
-        int middle_index = start_index +( end_index - start_index ) / 2 ; //Calculating middle index
-        merge_sort(array,start_index,middle_index);         //MergeSort for left array
-        merge_sort(array,middle_index+1,end_index);
-        merge(array,start_index,middle_index,end_index);
+        long int middle_index = start_index +( end_index - start_index ) / 2 ; //Calculating middle index
+        merge_sort(array,start_index,middle_index);         //MergeSort for left side of array
+        merge_sort(array,middle_index+1,end_index);         //MergeSort for right side of array
+        merge(array,start_index,middle_index,end_index);    //When sorten then merge them into one array
     }
 }
 
-
-template<typename Var>
-void swap(Var &first, Var &second )
-{
-
-        Var tmp = first;
-        first = second;
-        second = tmp;
-}
 
 template<typename Var>
 void quick_sort(Var *array,int start_index,int end_index)
@@ -112,6 +112,119 @@ void quick_sort(Var *array,int start_index,int end_index)
         quick_sort( array, i, end_index );
 
 }
+
+
+template<typename Var>
+int Partition(Var *array, int left, int right) {
+	int pivot = array[right];               // last index is PIVOT
+	int i = left;
+
+	//Elements lower than pivot will be pushed to the left side  of i ( INDEX)
+	//Elements higher than pivot will be pushed to the right side  of i ( INDEX)
+
+	// The most important is to move the pivot (i++) after swap
+	// because we want the element to be placed before PIVOT
+	for (int j = left; j < right; ++j)
+	{
+		if (array[j] <= pivot)
+		{
+		    swap(array[i],array[j]);
+			i++;
+		}
+	}
+
+	array[right] = array[i];
+	array[i] = pivot;
+
+	return i;           // returning index of PIVOT element
+}
+
+
+template<typename Var>
+void max_heapify(Var* array, int heapSize, int index) {
+	int left = (index + 1) * 2 - 1;
+	int right = (index + 1) * 2;
+	int largest = 0;
+
+	if (left < heapSize && array[left] > array[index])
+		largest = left;
+	else
+		largest = index;
+
+	if (right < heapSize && array[right] > array[largest])
+		largest = right;
+
+	if (largest != index)
+	{
+	    swap(array[index],array[largest]);
+		max_heapify(array, heapSize, largest);
+	}
+}
+
+template<typename Var>
+void heap_sort(Var* array, int size) {
+	int heapSize = size;
+
+	for(int p = (heapSize - 1) / 2; p >= 0; --p)
+		max_heapify(array, heapSize, p);
+
+	for(int i = size - 1; i > 0; --i)
+	{
+	    swap(array[i],array[0]);
+		--heapSize;
+		max_heapify(array, heapSize, 0);
+	}
+}
+
+
+template<typename Var>
+void insertion_sort(Var* array, int size) {
+	for (int i = 1; i < size; ++i)
+	{
+		int j = i;
+
+		while ((j > 0))
+		{
+			if (array[j - 1] > array[j])
+			{
+                swap(array[j-1],array[j]);
+				--j;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+}
+
+
+///////////////INTRO SORT////////////////////
+//  It is a merge of quick_sort, heap_sort //
+//  and my change is a insertion_sort when //
+//  array is really small ( less than 20)  //
+/////////////////////////////////////////////
+template<typename Var>
+void intro_sort(Var* array, int size) {
+	int partition_size = Partition(array, 0, size - 1);
+
+	if (partition_size < 20)             // if PIVOT INDEX is lover then 16 insertSort
+	{
+		insertion_sort(array, size);
+	}
+	else if (partition_size >(2 * log(size))) // if PIVOT INDEX is bigger than ___ then heap_sort is the most effective (GOOGLE KNOWLEDGE)
+	{
+		heap_sort(array, size);
+	}
+	else
+	{
+		quick_sort(array, 0, size - 1); // medium-size array then go quick_sort
+	}
+}
+
+
+
+
 
 
 
