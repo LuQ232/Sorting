b6e5a8dc1634490448a5ae91410f355540b678da
LuQ232 <LuQ232>
2020-03-07 18:12:04 +0100

    Merging both arrays done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "sort.hpp"
 #include "timer.hpp"
 
-#define NUMBER_OF_ARRAYS 100
+#define NUMBER_OF_ARRAYS 2
 
 
 void display_array(int *array,int size_of_array)
@@ -55,8 +55,18 @@ int main()
                 array_of_arrays[i][j]=rand() % 100000;
             }
         }
+        // TO NIE MOZE BYC TAK PROSTO JEDNAK :))))
 
+    std::cout<<"1st array: "<<std::endl;
+    display_array(array_of_arrays[0],size);
+    std::cout<<"2nd array: "<<std::endl;
+    display_array(array_of_arrays[1],size);
 
+    int *merged= new int [size*2];
+    merged = merge_arrays(array_of_arrays[0],array_of_arrays[1],size,size);
+       std::cout<<"MERGED "<<std::endl;
+    display_array(merged,size*2);
+   /*
 
         timer_of_all_sortings.start();  // START TIMER OF 100 SORTS
     for (int i=0; i<NUMBER_OF_ARRAYS; i++)
@@ -81,7 +91,7 @@ int main()
         //MERGE 56 SEKUND
         //QUICK 17 SEKUND
         //INTRO 52 SEKUND
-
+*/
         for (int i=0; i<NUMBER_OF_ARRAYS; i++)
         {
            delete [] array_of_arrays[i];
